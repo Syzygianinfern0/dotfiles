bcbce29294e2b6417cc880249b47a6bfb89c4869
Syzygianinfern0 <spsharan2000@gmail.com>
2023-06-21 18:16:02 -0700

    Plugins!

--- bin/mac_setup.sh
+++ bin/mac_setup.sh
@@ -4,3 +4,10 @@ brew install exa
 brew install dust
 brew install ffmpeg
 brew install neofetch
+brew tap teamookla/speedtest
+brew update
+brew install speedtest --force
+brew install duf
+brew install broot
+brew install fzf
+brew install rclone
