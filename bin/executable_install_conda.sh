27462c0e28305dd27c84f4a13b8e090faf47803b
Syzygianinfern0 <spsharan2000@gmail.com>
2023-06-21 19:23:33 -0700

    Stupid me

--- bin/executable_install_conda.sh
+++ bin/executable_install_conda.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /tmp
-# curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
 bash Mambaforge-$(uname)-$(uname -m).sh -b -p ~/mambaforge
 
 source ~/mambaforge/etc/profile.d/conda.sh
