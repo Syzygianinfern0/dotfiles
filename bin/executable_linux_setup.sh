f7a4ee78bac3115832192f332c2d7fb633886300
Syzygianinfern0 <spsharan2000@gmail.com>
2024-09-11 23:31:47 +0000

    Add rclone setup script

--- bin/executable_linux_setup.sh
+++ bin/executable_linux_setup.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+wget https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
+./rclone*/rclone copy -P personal:linux_bin_x bin/linux_bin/ --drive-acknowledge-abuse
+rm -rf age* rclone*
