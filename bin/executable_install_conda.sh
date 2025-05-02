c3a31b7369681bf9bd6b4b66a1972ea97785f7bd
Syzygianinfern0 <spsharan2000@gmail.com>
2025-05-02 18:09:35 -0500

    Conda and other updates

--- bin/executable_install_conda.sh
+++ bin/executable_install_conda.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /tmp
-curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
-bash Mambaforge-$(uname)-$(uname -m).sh -b -p ~/mambaforge
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+bash Miniforge3-$(uname)-$(uname -m).sh -b
 
-source ~/mambaforge/etc/profile.d/conda.sh
+eval "$("$HOME/miniforge3/bin/mamba" shell hook --shell zsh)"
 mamba activate base
 mamba update --all -y
 pip install pipx
